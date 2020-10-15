b73ccff9f63e83ade1fbd0843286410fa21e1d9e
Manuel <manuel.chichi@uner.edu.ar>
2020-10-15 19:28:07 -0300

    Testing new Worflow

--- init.rb
+++ init.rb
@@ -2,7 +2,7 @@ Redmine::Plugin.register :dss_pnrp do
   name 'Dss Pnrp plugin'
   author 'Author name'
   description 'Decision Support System'
-  version '0.0.2'
+  version '0.0.3'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
   menu :application_menu, :polls, { controller: 'polls', action: 'index' }, caption: 'Polls'
