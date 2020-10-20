5557d0f76711d96342660576405e61d9880b0a97
Manuel <manuel.chichi@uner.edu.ar>
2020-10-19 21:16:11 -0300

    Muestra solos los Issues del proyecto actual.

--- app/controllers/priorization_process_controller.rb
+++ app/controllers/priorization_process_controller.rb
@@ -1,10 +1,25 @@
 class PriorizationProcessController < ApplicationController
+  before_action :find_project, :authorize, only: :index
 
+  helper :queries
+  include QueriesHelper
+  
   def index
+    use_session = !request.format.csv?
+    retrieve_query(IssueQuery, use_session)
+
+    if @query.valid?
+        @issues = @query.issues()
+        render :layout => !request.xhr?
+    else
+        render :layout => !request.xhr?
+    end
+  rescue ActiveRecord::RecordNotFound
+    render_404
+  end
+
+  def find_project
     @project = Project.find(params[:project_id])
-
-    @issues = Issue.all
-
   end
   
   def init
--- init.rb
+++ init.rb
@@ -1,11 +1,16 @@
 Redmine::Plugin.register :dss_pnrp do
   name 'Dss Pnrp plugin'
-  author 'Author name'
+  author 'GICIIS'
   description 'Decision Support System'
   version '0.0.3'
   url 'http://example.com/path/to/plugin'
   author_url 'http://example.com/about'
   menu :application_menu, :polls, { controller: 'polls', action: 'index' }, caption: 'Polls'
+
+  
   permission :priorization_process, { priorization_process: [:index, :vote] }, public: true
+
+  #Usar PROC para que solo se pueda ver si es el raiz. 
+  # :if => Proc.new { Project.current.root? }
   menu :project_menu, :priorization_process, { controller: 'priorization_process', action: 'index' }, caption: 'Priorization Process', param: :project_id
 end
