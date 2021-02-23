1c6f491f0bf7ed24848f6a11c25400e4b469add8
Manuel Chichi <manuelchichi96@gmail.com>
2021-02-23 18:31:29 -0300

    Routas

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,17 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 
-get 'priorization_process', to: 'priorization_process#index'
-get 'priorization_process_get', to: 'priorization_process#get'
+
+get '/priorization_process_get', to: 'priorization_process#get'
+
+get '/priorization_process', to: 'priorization_process#index'
+get '/priorization_process/:id', to: 'priorization_process#show'
+post '/priorization_process', to: 'priorization_process#add'
+put '/priorization_process/:id', to: 'priorization_process#modify'
+delete '/priorization_process/:id', to: 'priorization_process#remove'
+
+get '/nrp_process', to: 'nrp_process#index'
+get '/nrp_process/:id', to: 'nrp_process#show'
+post '/nrp_process', to: 'nrp_process#add'
+put '/nrp_process/:id', to: 'nrp_process#modify'
+delete '/nrp_process/:id', to: 'nrp_process#remove'
