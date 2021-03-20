d400a5502daa140196d300294f62c5620d319a7e
Manuel Chichi <manuelchichi96@gmail.com>
2021-03-20 19:30:32 -0300

    Feature: Reestrucutracion de las rutas y agregados controladores.

---
+++ app/helpers/next_release_process_helper.rb
@@ -0,0 +1,2 @@
+module NextReleaseProcessHelper
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,11 @@
 # See: http://guides.rubyonrails.org/routing.html
 
 
-get '/priorization_process_get', to: 'priorization_process#get'
+get '/projects/:project_id/requeriment_engineering/index', to: 'requeriment_engineering#index'
+
+get '/priorization_process_persons', to: 'priorization_process#persons'
 get '/priorization_process_algorithms', to: 'priorization_process#algorithms'
+get '/priorization_process_alternatives', to: 'priorization_process#alternatives'
 
 get '/priorization_process', to: 'priorization_process#index'
 get '/priorization_process/:id', to: 'priorization_process#show'
@@ -11,8 +14,8 @@ post '/priorization_process', to: 'priorization_process#add'
 put '/priorization_process/:id', to: 'priorization_process#modify'
 delete '/priorization_process/:id', to: 'priorization_process#remove'
 
-get '/nrp_process', to: 'nrp_process#index'
-get '/nrp_process/:id', to: 'nrp_process#show'
-post '/nrp_process', to: 'nrp_process#add'
-put '/nrp_process/:id', to: 'nrp_process#modify'
-delete '/nrp_process/:id', to: 'nrp_process#remove'
+get '/next_release_process', to: 'next_release_process#index'
+get '/next_release_process/:id', to: 'next_release_process#show'
+post '/next_release_process', to: 'next_release_process#add'
+put '/next_release_process/:id', to: 'next_release_process#modify'
+delete '/next_release_process/:id', to: 'next_release_process#remove'
