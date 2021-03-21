3d21d22f21aa5d373f0ec7cf08b5a8c634d78f87
Manuel Chichi <manuelchichi96@gmail.com>
2021-03-20 22:02:38 -0300

    feature: Estructura de los procesos de priorizacion.

--- db/migrate/20201008232224_create_priorization_processes.rb
+++ db/migrate/20201008232224_create_priorization_processes.rb
@@ -1,8 +1,39 @@
 class CreatePriorizationProcesses < ActiveRecord::Migration[5.2]
   def change
     create_table :priorization_processes do |t|
-      t.string :alternatives
-      t.boolean :finished
+      t.integer :project_id
+      t.timestamp :created_on
+      t.timestamp :updated_on
+      t.integer :status
     end
+    
+    create_table :pp_related_issues do |t|
+      t.integer :priorization_process_id
+      t.integer :issue_id
+      t.integer :old_priority
+      t.integer :new_priority
+      t.integer :status
+    end
+
+    create_table :pp_criteria do |t|
+      t.integer :priorization_process_id
+      t.string :name
+      t.string :description
+      t.integer :field_id
+    end
+
+    create_table :pp_decision_makers do |t|
+      t.integer :priorization_process_id
+      t.integer :user_id
+      t.boolean :admin
+    end
+
+    create_table :pp_criteria_ponderation do |t|
+      t.integer :pp_criteria_id
+      t.integer :user_id
+      t.integer :value
+    end
+
+
   end
 end
