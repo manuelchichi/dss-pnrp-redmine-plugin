64a71feec8ba6889c69acd01b13137f369e32619
LucasMartinPrado <prado.lucasm@gmail.com>
2021-04-02 20:36:17 -0300

    feature: agregadas las versiones iniciales de tablas de priorizacion y proximo lanzamiento

--- app/controllers/requeriment_engineering_controller.rb
+++ app/controllers/requeriment_engineering_controller.rb
@@ -1,6 +1,14 @@
 class RequerimentEngineeringController < ApplicationController
+  before_action :find_project
 
   def index
+    @name  = "Vista de procesos y proximas entregas"
+    @issues1 = Issue.all  #Priorizacion
+    @issues2 = Issue.all  #ProximoLanzamiento
+  end
+
+  def find_project
+    @project = Project.find(params[:project_id])
   end
 
   def show
